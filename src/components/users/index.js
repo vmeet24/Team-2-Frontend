ecc9b18e09f8d2a6477d49e4a40710037bb355c2
Ernie Gurish <gurish.e@northeastern.edu>
2022-11-25 20:55:47 -0500

    Adding users component + user row for the list

---
+++ src/components/users/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {findAllUsers, deleteUser} from "../../services/users-service";
+import * as service from "../../services/auth-service";
+import {useEffect, useState} from "react";
+import {useLocation, useParams, Link} from "react-router-dom";
+import UserRow from './user-row'
+
+const Users = () => {
+    const [profile, setProfile] = useState(null);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const user = await service.profile();
+                setProfile(user);
+                console.log(profile)
+            } catch (e) {
+                console.log('No user logged in')
+            }
+        }
+
+        const fetchUsers = async () => {
+            try {
+                const usersList = await findAllUsers();
+                setUsers(usersList);
+            } catch (e) {
+                alert('Error fetching user info');
+            }
+        }
+        fetchProfile();
+        fetchUsers();
+    }, []);
+
+    return (
+        <>
+            <h1>Tuiter Users</h1>
+            <h3>Administrators:</h3>
+            <div className="list-group mb-3">
+                {users.filter(user => user.admin).map(user => 
+                    <UserRow key={user._id} user={user} profile={profile} />
+                )}
+            </div>
+            <h3>Users:</h3>
+            <div className="list-group">
+                {users.filter(user => !user.admin).map(user =>
+                    <UserRow key={user._id} user={user} profile={profile} />
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Users

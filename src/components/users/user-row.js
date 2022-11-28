33be20babd9109e707bf809096d6b0e4d7a3a968
Ernie Gurish <gurish.e@northeastern.edu>
2022-11-28 17:30:18 -0500

    Add Make/Revoke Admin button to UserRow and refresh list on change

--- src/components/users/user-row.js
+++ src/components/users/user-row.js
@@ -1,14 +1,29 @@
 import React, {useState} from "react";
-import {deleteUser} from "../../services/users-service";
+import {useNavigate} from "react-router-dom";
+import {deleteUser, updateUser} from "../../services/users-service";
+import {logout} from "../../services/auth-service";
 
-const UserRow = ({user, profile}) => {
+const UserRow = ({user, profile, refresh}) => {
     const [deleted, setDeleted] = useState(false);
-    
+    const navigate = useNavigate();
+
+    // on success, just hide the user from the list rather than refreshing to save API calls
     const handleDelete = async (id) => {
         setDeleted(true);
         const { deletedCount } = await deleteUser(id);
-        if (!deletedCount || deletedCount === 0) {
+        if (id === profile._id) { // if we delete ourselves, log out
+            logout();
+            navigate('/login');
+        } else if (!deletedCount || deletedCount === 0) {
             setDeleted(false);
+        }
+    }
+
+    // refresh here to show that updated status went through
+    const toggleAdmin = async () => {
+        const { modifiedCount } = await updateUser(user._id, { admin: !user.admin })
+        if (modifiedCount && modifiedCount > 0) {
+            refresh();
         }
     }
 
@@ -18,13 +33,18 @@ const UserRow = ({user, profile}) => {
             <button
                 disabled={!profile || !profile.admin || deleted}
                 hidden={!profile || !profile.admin}
-                onClick={(e) => {
-                    e.stopPropagation()
-                    e.preventDefault()
-                    handleDelete(user._id)
-                }}
-                className="btn btn-danger fa-pull-right">
+                onClick={(e) => handleDelete(user._id) }
+                className="btn btn-danger fa-pull-right"
+            >
                 <i className="fas fa-remove"></i>
+            </button>
+            <button
+                disabled={!profile || !profile.admin}
+                hidden={!profile || !profile.admin || profile._id === user._id}
+                onClick={toggleAdmin}
+                className="btn btn-warning fa-pull-right"
+            >
+                {user.admin ? 'Revoke Admin' : 'Make Admin'}
             </button>
         </div>
     )

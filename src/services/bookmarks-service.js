7393d3c2f62cb08b2025a59822422ae68e4af68e
prinjal <prinjal.dave@gmail.com>
2022-12-08 22:01:21 -0500

    changes in bookmarks-service regarding api end point and tests for bookmarks-service

--- src/services/bookmarks-service.js
+++ src/services/bookmarks-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_LOCAL_URL
-const USERS_API = `${BASE_URL}/auth`
+const USERS_API = `${BASE_URL}/users`
 
 const api = axios.create({
     withCredentials: true
@@ -11,10 +11,11 @@ export const userTogglesTuitBookmarks = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/bookmarks/${tid}`)
         .then(response => response.data)
 
+
 export const findAllTuitsBookmarkedByUser = (uid) =>
     api.get(`${USERS_API}/${uid}/bookmarks`)
         .then(response => response.data)
 
 export const findAllBookmarks = () =>
     api.get(`${BASE_URL}/api/bookmarks`)
-      .then(response => response.data)
+        .then(response => response.data)

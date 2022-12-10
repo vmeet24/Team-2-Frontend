afbcb1187c99b128ca3fa6374ddcc91d04305945
Ernie Gurish <gurish.e@northeastern.edu>
2022-12-09 19:57:06 -0500

    Include bookmark button on tuits

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import * as service from "../../services/bookmarks-service"
 
-export default class TuitStats extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+const TuitStats = ({tuit, user, refreshTuits}) => {
+    const [isBookmarked, setBookmarked] = useState(false);
+
+    const bookmarkTuit = () => {
+        service.userTogglesTuitBookmarks(user._id, tuit._id)
+            .then(result => {
+                setBookmarked(true);
+                refreshTuits();
+            })
+    }
+
     return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.replies}
+        <div className="row mt-2">
+            <div className="col">
+                <i className="far fa-message me-1"></i>
+                {tuit.stats && tuit.stats.replies}
+            </div>
+            <div className="col">
+                <i className="far fa-retweet me-1"></i>
+                {tuit.stats && tuit.stats.retuits}
+            </div>
+            <div className="col">
+                <i className="far fa-heart me-1"></i>
+                {tuit.stats && tuit.stats.likes}
+            </div>
+            <div className="col">
+                <i className="far fa-inbox-out"></i>
+            </div>
+            <div className="col">
+                <span onClick={bookmarkTuit}>
+                    {isBookmarked &&
+                        <i className="fa-solid fa-bookmark"></i>
+                    }
+                    {!isBookmarked &&
+                        <i className="fa-regular fa-bookmark"></i>
+                    }
+                </span>
+            </div>
         </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.retuits}
-        </div>
-        <div className="col">
-          <i className="far fa-heart me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.likes}
-        </div>
-        <div className="col">
-          <i className="far fa-inbox-out"></i>
-        </div>
-      </div>
     );
-  }
 }
+
+export default TuitStats;
